099c5ffb54072ea7aed88e542d084e1110a5dfec
kmbruce1986 <kmbruce1986@gmail.com>
2018-09-24 16:38:46 +0100

    adds tests for water bottle

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -2,14 +2,23 @@ public class WaterBottle {
     private int volume;
 
     public WaterBottle(int volume){
-        this.volume = volume;
+        this.volume = 100;
     }
 
     public int getVolume() {
         return volume;
     }
 
-    public void setVolume(int volume) {
-        this.volume = volume;
+    public void drink(){
+        this.volume -= 10;
+    }
+
+
+    public int emptyBottle(){
+        return this.volume = 0;
+    }
+
+    public int fillBottle(){
+        return this.volume = 100;
     }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,50 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void canGetVolume(){
+        int volume = waterBottle.getVolume();
+        assertEquals(100, volume);
+    }
+
+    @Test
+    public void canDrink(){
+        waterBottle.drink();
+        int volume = waterBottle.getVolume();
+        assertEquals(90, volume);
+    }
+
+    @Test
+    public void canHaveMultipleDrinks(){
+        waterBottle.drink();
+        waterBottle.drink();
+        int volume = waterBottle.getVolume();
+        assertEquals(80, volume);
+    }
+
+    @Test
+    public void canEmptyBottle(){
+        waterBottle.emptyBottle();
+        int volume = waterBottle.getVolume();
+        assertEquals(0, volume);
+    }
+
+    @Test
+    public void canFillBottle(){
+        waterBottle.emptyBottle();
+        waterBottle.fillBottle();
+        int volume = waterBottle.getVolume();
+        assertEquals(100, volume);
+    }
+
 }
